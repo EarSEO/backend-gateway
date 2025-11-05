8dd18b9c9e2346591cfd2315073c543aa174abee
윤호 남 <nam2660433@naver.com>
2025-11-06 05:34:01 +0900

    Initial commit

---
+++ src/test/java/com/earseo/backendgateway/BackendGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earseo.backendgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
